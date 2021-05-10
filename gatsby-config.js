9c17979801ae048cfba85613eb0281161451bc6a
Jeffrey Scott Martin - Sulo Labs <sulo.labs@gmail.com>
2021-05-10 10:58:28 -0400

    added sdt logo and began sidbar setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     defaultTitle: `Rocket Docs`,
     siteTitleShort: `Rocket Docs`,
     siteDescription: `Out of the box Gatsby Theme for creating documentation websites easily and quickly`,
-    siteUrl: `https://rocketdocs.netlify.app`,
+    siteUrl: `https://sdtdocs.netlify.app`,
     siteAuthor: `@rocketseat`,
     siteImage: `/banner.png`,
     siteLanguage: `en`,
